8188d29e98ed003d17362e7e33ded6bc580ba1e1
KiranMahn <68765104+KiranMahn@users.noreply.github.com>
2025-08-05 11:24:50 +0100

    update comments in text-parser

--- text-parser.go
+++ text-parser.go
@@ -7,14 +7,19 @@ import (
 	"strings"
 )
 
+// takes a filepath to a list of stopwords in a text file and returns a map of stopwords
 func LoadStopwords(filePath string) (map[string]struct{}, error) {
+	// make empty map
 	stopwords := make(map[string]struct{})
+
+	// open the file
 	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, errors.New("Error opening file, check file path: " + err.Error())
 	}
 	defer file.Close()
 
+	// create a new scanner and for each line in the file, add the word to the stopwords map
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		word := strings.TrimSpace(scanner.Text())
@@ -26,6 +31,7 @@ func LoadStopwords(filePath string) (map[string]struct{}, error) {
 		return nil, err
 	}
 
+	// if there are no stopwords in the file, return an error
 	if len(stopwords) == 0 {
 		return nil, errors.New("no stopwords found in file")
 	}
